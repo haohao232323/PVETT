ed6db19f5acce6dfe08d7166de74bcbf920cc922
haohao232323 <42701714+haohao232323@users.noreply.github.com>
2023-03-28 17:10:48 +0800

    Add files via upload

---
+++ 10go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Clone the GitHub repo
+git clone https://github.com/haohao232323/PVETT/blob/main/PVETT.sh
+# Change to the script directory
+cd /root
+# Make the script executable
+chmod +x run_check_server_status.sh
+# Run the script
+./run_check_server_status.sh
